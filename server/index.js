6a5c5255e7d0dbe8f6322537fffc514f80436114
unknown <neel26ray@gmail.com>
2025-11-30 19:57:11 +0530

    ok2

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const { run } = require("./config/db.config.js"); 
 const cors = require("cors");
@@ -5,11 +6,18 @@ const authRoute = require("./routers/authroute");
 const courseRoute = require("./routers/course.route");
 const moduleRoute = require("./routers/module.route");
 const lessonRoute = require("./routers/lesson.route");
+const { configDotenv } = require("dotenv");
 const app = express();
 app.use(express.json());
-const port = 5000;
+const port =  process.env.PORT || 5000;
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: allowedOrigins,
   credentials: true,
 }));
 app.get("/api/health", (req, res) => {
